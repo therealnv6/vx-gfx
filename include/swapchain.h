72b2caed2d4d56d2675ef8db5cc537d7b7a3f663
therealnv6 <devrawr@protonmail.com>
2023-05-06 07:50:11 +0200

    add some basic documentation in swapchain.h

--- include/swapchain.h
+++ include/swapchain.h
@@ -2,42 +2,55 @@
 
 #define GLFW_INCLUDE_VULKAN
 
-#include <functional>
-
 #include <GLFW/glfw3.h>
 #include <device.h>
+#include <functional>
 #include <global.h>
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vulkan_structs.hpp>
 
 namespace gfx
 {
+    // A structure that holds the swapchain's support details.
     struct swapchain_support_details
     {
-        vk::SurfaceCapabilitiesKHR capabilities;
-        std::vector<vk::SurfaceFormatKHR> formats;
-        std::vector<vk::PresentModeKHR> present_modes;
+        vk::SurfaceCapabilitiesKHR capabilities; // The surface's capabilities.
+        std::vector<vk::SurfaceFormatKHR> formats; // The surface's available formats.
+        std::vector<vk::PresentModeKHR> present_modes; // The surface's available present modes.
 
+        // Returns true if the required details are complete.
         bool is_complete()
         {
             return !this->formats.empty() && !this->present_modes.empty();
         }
     };
 
+    // Queries the swapchain's support details for a given device and surface.
     gfx::swapchain_support_details query_swapchain_support(std::optional<vk::PhysicalDevice> device, vk::SurfaceKHR surface);
 
+    /**
+     * The `swapchain` class manages a Vulkan swapchain, which is responsible for presenting rendered images to a window surface.
+     *
+     * It provides functionality to create and manage the swapchain,
+     including querying and selecting the optimal image format and presentation mode.
+     *
+     * @see `gfx::device` - The device class handles device initialization, and can be reused across multiple contexts.
+     * @see `vk::SwapchainKHR` - The low-level vulkan swapchain which this object wraps around.
+     */
     class swapchain
     {
     public:
-        std::vector<vk::Image> images;
-        std::vector<vk::ImageView> image_views;
+        std::vector<vk::Image> images; // The swapchain's images.
+        std::vector<vk::ImageView> image_views; // The swapchain's image views.
 
-        vk::SwapchainKHR chain;
-        vk::Format image_format;
-        vk::Extent2D extent;
+        vk::SwapchainKHR chain; // The Vulkan swapchain object.
+        vk::Format image_format; // The format of the swapchain's images.
+        vk::Extent2D extent; // The extent of the swapchain's images.
 
+        // Function for choosing a swap surface format.
         std::function<gfx::surface_format(gfx::surface_formats &available_formats)> choose_swap_surface = [](gfx::surface_formats &available_formats)
         {
+            // Chooses the first available format that matches the desired format.
             for (const auto &available_format : available_formats)
             {
                 if (available_format.format == vk::Format::eB8G8R8A8Srgb && available_format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear)
@@ -46,27 +59,31 @@ namespace gfx
                 }
             }
 
+            // Returns the first available format if no matching format is found.
             return available_formats[0];
         };
 
         // Function for choosing a present mode.
-        // This function takes a vector of available present modes as input
-        // and returns a chosen present mode.
         std::function<gfx::present_mode(const gfx::present_modes &available_present_modes)> choose_present_mode = [](const gfx::present_modes &available_present_modes)
         {
             // Default present mode if no other is chosen.
             return gfx::present_mode::eImmediate;
         };
 
+        // Initializes the swapchain object.
         void initialize(GLFWwindow *window, vk::SurfaceKHR &surface);
+
+        // Creates the swapchain object.
         void create_swapchain(vk::SurfaceKHR &surface, gfx::swapchain_support_details details);
 
+        // Chooses the swap extent based on the surface's capabilities and the window's size.
         vk::Extent2D choose_swap_extent(const vk::SurfaceCapabilitiesKHR &capabilities, GLFWwindow *window);
 
+        // Constructor for the swapchain class.
         swapchain(gfx::device *device)
             : device { device } {};
 
     private:
-        gfx::device *device;
+        gfx::device *device; // The device associated with the swapchain.
     };
 }
