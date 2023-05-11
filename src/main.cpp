0ee0c99270ffb86e4ed92b570afedbee058fc1c1
therealnv6 <devrawr@protonmail.com>
2023-05-11 02:25:51 +0200

    add some documentation to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,28 +9,41 @@
 #include <util.h>
 #include <vertex.h>
 
+// initialize graphics context, device and swapchain
 int main()
 {
     try
     {
+        // create shared context object
         auto context = std::make_shared<gfx::context>();
 
+        // create shared device object
         auto device = std::make_shared<gfx::device>(&context->instance, &context->surface);
+
+        // create shared swapchain object
         auto swapchain = std::make_shared<gfx::swapchain>(device);
 
-        // we have to initialize the swapchain __before__ we do anything else with the swapchain!
+        // initialize swapchain before doing anything else with it
         context->init_swap_chain(swapchain);
-        swapchain->add_render_pass(
-            "shadow", gfx::start_render_pass(swapchain) // add a new render_pass to the swapchain by name "shadow"
-        );
 
+        // add new render pass to swapchain by name "shadow"
+        swapchain->add_render_pass(
+            "shadow", gfx::start_render_pass(swapchain));
+
+        // get the render pass from the swapchain
         auto render_pass = swapchain->render_passes.at("shadow");
+
+        // create shared commands object
         auto commands = std::make_shared<gfx::commands>(swapchain, &context->surface);
 
+        // set device and commands for the context
         context->device = device;
         context->commands = commands;
 
+        // create draw object
         gfx::draw drawer(context);
+
+        // create pipeline object with specified parameters
         gfx::pipeline pipeline {
             swapchain,
             "shadow",
@@ -38,7 +51,7 @@ int main()
             "build/triangle.frag.spv",
         };
 
-        // todo: think of a more elegant way to do this.
+        // bind vertex buffer and attribute descriptions
         // clang-format off
         pipeline.bind_struct<gfx::vertex>(
             gfx::vertex::get_binding_description(),
@@ -46,27 +59,47 @@ int main()
         );
         // clang-format on
 
+        // initialize the pipeline object
         pipeline.initialize();
 
+        // create vertex data
         const std::vector<gfx::vertex> vertices = {
             {{ 0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
             { { 0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f }},
             {{ -0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f }}
         };
 
+        // create vertex buffer object
         gfx::vertex_buffer<gfx::vertex, 3> vertex_buffer(device, vertices);
 
+        // render loop
         while (!glfwWindowShouldClose(context->window))
         {
+            // begin drawing commands
             drawer.begin();
+
+            // run commands within the draw object
             drawer.run([&](vk::CommandBuffer *buffer, auto index) {
+                // begin render pass
                 render_pass.begin(buffer, index, gfx::clear({ 0.0, 0.0, 0.0, 1.0 }));
+
+                // bind pipeline
                 buffer->bindPipeline(vk::PipelineBindPoint::eGraphics, pipeline.vk_pipeline);
+
+                // bind vertex buffer
                 buffer->bindVertexBuffers(0, vertex_buffer.get_buffer(), { 0 });
+
+                // draw vertices
                 buffer->draw(static_cast<uint32_t>(vertices.size()), 1, 0, 0);
-                render_pass.end(buffer); // we have to end the render pass!
+
+                // end render pass
+                render_pass.end(buffer);
             });
+
+            // poll for events
             glfwPollEvents();
+
+            // wait for device to finish rendering
             device->logical_device.waitIdle();
         }
     } catch (std::exception &e)
