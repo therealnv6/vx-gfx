362d6fb4be1e2f66feddd0b1c433992d6808f1d9
therealnv6 <devrawr@protonmail.com>
2023-05-19 08:53:23 +0200

    merge image.cpp into buffer.h

--- include/buffer/buffer.h
+++ include/buffer/buffer.h
@@ -85,5 +85,60 @@ namespace gfx
 		~vec_buffer() {};
 	};
 
+	template<>
+	class buffer<VkImage>
+	{
+	public:
+		buffer(std::shared_ptr<gfx::device> device, std::shared_ptr<gfx::commands> commands, VkImage image, vk::ImageUsageFlags usage, vma::memory_usage memory_usage)
+			: device(device)
+			, commands(commands)
+			, image(image)
+		{
+			create_device_image(usage, memory_usage);
+		}
+		~buffer()
+		{
+			destroy_image();
+		}
+
+	private:
+		void create_device_image(vk::ImageUsageFlags usage, vma::memory_usage memory_usage)
+		{
+			vk::ImageCreateInfo image_info {
+				{},
+				vk::ImageType::e2D,
+				vk::Format::eUndefined,
+				vk::Extent3D(1, 1, 1),
+				1,
+				1,
+				vk::SampleCountFlagBits::e1,
+				vk::ImageTiling::eOptimal,
+				usage,
+				vk::SharingMode::eExclusive,
+			};
+
+			VmaAllocationCreateInfo alloc_info = { 0, vma::to_vma_memory_usage(memory_usage) };
+			VmaAllocation allocation;
+
+			VkImageCreateInfo device_info = static_cast<VkImageCreateInfo>(image_info);
+
+			vmaCreateImage(device->get_vma_allocator(),
+				&device_info,
+				&alloc_info,
+				&image,
+				&allocation,
+				nullptr);
+		}
+		void destroy_image()
+		{
+			vmaDestroyImage(device->get_vma_allocator(), image, allocation);
+		}
+
+		std::shared_ptr<gfx::device> device;
+		std::shared_ptr<gfx::commands> commands;
+
+		VkImage image;
+		VmaAllocation allocation;
+	};
 	template class gfx::vec_buffer<gfx::vertex>;
 }
